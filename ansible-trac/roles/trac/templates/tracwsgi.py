b8acdd75e253ed5fffd01c1d82c2d5f48edd1487
hliblack <hliblack@163.com>
2019-02-22 15:06:01 +0800

    v1.0.4

--- ansible-trac/roles/trac/templates/tracwsgi.py
+++ ansible-trac/roles/trac/templates/tracwsgi.py
@@ -1,17 +1,19 @@
 import sys
 import os
-import trac.web.main
 
-def application(environ, start_request):
-    if not 'trac.env_parent_dir' in environ:
-        environ.setdefault('trac.env_path', '/data/wwwroot/trac')
-    if 'PYTHON_EGG_CACHE' in environ:
-        os.environ['PYTHON_EGG_CACHE'] = environ['PYTHON_EGG_CACHE']
-    elif 'trac.env_path' in environ:
-        os.environ['PYTHON_EGG_CACHE'] = \
-            os.path.join(environ['trac.env_path'], '.egg-cache')
-    elif 'trac.env_parent_dir' in environ:
-        os.environ['PYTHON_EGG_CACHE'] = \
-            os.path.join(environ['trac.env_parent_dir'], '.egg-cache')
-    from trac.web.main import dispatch_request
-    return dispatch_request(environ, start_request)
+sys.stdout = sys.stderr
+
+# put here your ENV variables
+# here is an example with multiple instances
+os.environ['TRAC_ENV_PARENT_DIR'] = '/data/wwwroot/'
+os.environ['PYTHON_EGG_CACHE'] = '/data/www/tracroot/.eggs/'
+
+import trac.web.main
+from trac.web.standalone import AuthenticationMiddleware
+from trac.web.main import dispatch_request
+from trac.web.auth import BasicAuthentication
+
+def application(environ, start_application):
+    auth = {"*" : BasicAuthentication("/etc/nginx/conf.d/users", "admin")}
+    wsgi_app = AuthenticationMiddleware(dispatch_request, auth)
+    return wsgi_app(environ, start_application)
